1fe7e37ef07ad2235124acf02991cecd0a7a61c9
Alex Dhaenens <aldhaenens@reference.be>
2020-02-29 11:52:13 +0100

    Adding user registration

---
+++ src/Feature/Attendees/code/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Feature.Attendees")]
+[assembly: Guid("a908ac5b-cc32-4977-bde0-cc232afe2b1a")]
---
+++ src/Feature/Attendees/code/SubmitActions/RegisterAttendee.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Web.Security;
+using Sitecore.Data;
+using Sitecore.ExperienceForms.Models;
+using Sitecore.ExperienceForms.Processing;
+using Sitecore.ExperienceForms.Processing.Actions;
+using Sitecore.Security.Accounts;
+using Sitecore.Security.Domains;
+
+namespace Feature.Attendees.SubmitActions
+{
+    public class RegisterAttendee : SubmitActionBase<string>
+    {
+        public RegisterAttendee(ISubmitActionData submitActionData) : base(submitActionData)
+        {
+        }
+
+        protected override bool Execute(string data, FormSubmitContext formSubmitContext)
+        {
+
+            try
+            {
+                string userName = GetFormValue(formSubmitContext, "username");
+                string email = GetFormValue(formSubmitContext, "email");
+                string password = GetFormValue(formSubmitContext, "password");
+                string firstName = GetFormValue(formSubmitContext, "firstname");
+                string lastName = GetFormValue(formSubmitContext, "lastname");
+                if (!User.Exists(userName))
+                {
+                    Membership.CreateUser($"extranet\\{userName}", password, email);
+                    // Edit the profile information
+                    var user = User.FromName($"extranet\\{userName}", true);
+                    var userProfile = user.Profile;
+                    userProfile.FullName = string.Format("{0} {1}", firstName, lastName);
+
+                    userProfile.Save();
+                }
+                else
+                {
+                    Sitecore.Diagnostics.Log.Error("User exist", this);
+                    return false;
+                }
+            }
+            catch (MembershipCreateUserException ex)
+            {
+                Sitecore.Diagnostics.Log.Error("Message", ex, this);
+            }
+
+            return true;
+        }
+
+        protected override bool TryParse(string value, out string target)
+        {
+            target = string.Empty;
+            return true;
+        }
+
+        private static string GetFormValue(FormSubmitContext formSubmitContext, string formFieldName)
+        {
+            var field = formSubmitContext.Fields.FirstOrDefault(f => f.Name.Equals(formFieldName, StringComparison.InvariantCultureIgnoreCase));
+            return field?.GetType().GetProperty("Value")?.GetValue(field, null)?.ToString() ?? string.Empty;
+        }
+    }
+}
