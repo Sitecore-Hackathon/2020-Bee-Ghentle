0d1ebc1ba75f12c030c333d88c9936cd377d347f
Alex Dhaenens <aldhaenens@reference.be>
2020-03-01 00:18:03 +0100

    Adding subscribers

---
+++ src/Feature/Attendees/code/Areas/Attendees/Controllers/AttendeesController.cs
@@ -0,0 +1,48 @@
+﻿using System.Web.Mvc;
+using Sitecore.Data;
+using Sitecore.SecurityModel;
+
+namespace Feature.Attendees.Areas.Attendees.Controllers
+{
+    public class AttendeesController : Controller
+    {
+        public ActionResult SubscribeToEvent()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SubscribeToEvent(string param)
+        {
+            using (new SecurityDisabler())
+            {
+                var page = Sitecore.Context.Item;
+
+                var username = Sitecore.Security.Accounts.User.Current.IsAuthenticated
+                    ? Sitecore.Security.Accounts.User.Current.Name : null;
+
+                string currentValue = page["subscribers"];
+                if (!string.IsNullOrEmpty(username))
+                {
+                    string newEntry = $"{username.Remove(0, 9)}";
+                    if (currentValue == null || !currentValue.Contains(newEntry))
+                    {
+                        string newVal = currentValue + "|" + newEntry;
+
+                        page.Editing.BeginEdit();
+                        page["subscribers"] = newVal;
+                        page.Editing.EndEdit();
+
+                        var master = Sitecore.Configuration.Factory.GetDatabase("master");
+                        var pageMaster = master.GetItem(page.ID);
+
+                        pageMaster.Editing.BeginEdit();
+                        pageMaster["subscribers"] = newVal;
+                        pageMaster.Editing.EndEdit();
+                    }
+                }
+            }
+            return new EmptyResult();
+        }
+    }
+}
---
+++ src/Feature/Attendees/code/Fields/HiddenContextItemViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Sitecore.Data.Items;
+using Sitecore.ExperienceForms.Mvc.Models.Fields;
+
+namespace Feature.Attendees.Fields
+{
+    [Serializable]
+    public class HiddenContextItemViewModel : InputViewModel<string>
+    {
+        public string ContextItemId { get; set; }
+        public HiddenContextItemViewModel()
+        {
+            if (string.IsNullOrEmpty(ContextItemId))
+            {
+                ContextItemId = Sitecore.Context.Item.ID.Guid.ToString();
+            }
+        }
+        protected override void InitItemProperties(Item item)
+        {
+            base.InitItemProperties(item);
+        }
+
+        protected override void UpdateItemFields(Item item)
+        {
+            base.UpdateItemFields(item);
+        }
+
+    }
+}
